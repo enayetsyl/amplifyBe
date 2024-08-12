02a8863c902e5c0a4b5feb018143270a4d4e77ff
coderHarsh121212 <codesource287@gmail.com>
2024-08-12 18:19:48 +0530

    new updates

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Connected to the database ");
+    console.log("Connected to the database");
   })
   .catch((err) => {
     console.error(`Error connecting to the database. n${err}`);
