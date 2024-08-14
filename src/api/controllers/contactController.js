0e1c5eedc7b6fceb32ec77636d8e95790e3344cd
enayetaitech <mdrehman@365aitech.com>
2024-08-14 16:25:06 +0600

    update contact function refactoring done

--- src/api/controllers/contactController.js
+++ src/api/controllers/contactController.js
@@ -4,23 +4,20 @@ const { validationResult } = require("express-validator");
 
 // Controller to create a new project
 const createContact = async (req, res) => {
-  const { firstName, lastName, email, companyName, roles, createdBy  } = req.body;
+  const { firstName, lastName, email, companyName, roles, createdBy } = req.body;
 
-  console.log('req body', req.body); // Log the request body to the console
-
- // Validation to check if all required fields are present
- if (!firstName || !lastName || !email || !companyName || !roles || !createdBy) {
-  return res.status(400).json({
-    message: "All fields are required: firstName, lastName, email, companyName, roles, createdBy."
-  });
-}
+  // Validation to check if all required fields are present
+  if (!firstName || !lastName || !email || !companyName || !roles || !createdBy) {
+    return res.status(400).json({
+      message: "All fields are required: firstName, lastName, email, companyName, roles, createdBy."
+    });
+  }
 
   try {
-     const newContact = new Contact({
+    const newContact = new Contact({
       firstName, lastName, email, companyName, roles, createdBy
     });
 
-    console.log("saved", newContact);
     const savedContact = await newContact.save();
     res.status(201).json(savedContact);
   } catch (error) {
@@ -31,14 +28,13 @@ const createContact = async (req, res) => {
 
 // Controller to get all projects with pagination
 const getAllContacts = async (req, res) => {
- 
+
   const { page = 1, limit = 10 } = req.query; // Default to page 1 and 10 items per page
 
   try {
     const contacts = await Contact.find()
       .skip((page - 1) * limit) // Skip the appropriate number of documents
       .limit(parseInt(limit)); // Limit the number of documents
-console.log('all contacts', contacts)
     const totalDocuments = await Contact.countDocuments(); // Total number of documents in collection
     const totalPages = Math.ceil(totalDocuments / limit); // Calculate total number of pages
 
@@ -71,55 +67,26 @@ const getContactById = async (req, res) => {
 const updateContact = async (req, res) => {
   const { id } = req.params;
   const {
-    name,
-    description,
-    startDate,
-    status,
-    startTime,
-    timeZone,
-    participants,
-    observers,
-    breakoutRooms,
-    polls,
-    interpreters,
-    passcode,
-    endDate,
+    firstName, lastName, email, companyName, roles
   } = req.body;
 
   try {
-    // Hash the passcode using bcryptjs if provided
-    let hashedPasscode = passcode;
-    if (passcode) {
-      hashedPasscode = await bcrypt.hash(passcode, 8); // Adjust saltRounds as per your security requirements
-    }
 
-    const updatedProject = await Project.findByIdAndUpdate(
+    const updatedContact = await Contact.findByIdAndUpdate(
       id,
       {
-        name,
-        description,
-        startDate,
-        status,
-        startTime,
-        timeZone,
-        participants: participants || [],
-        observers: observers || [],
-        breakoutRooms: breakoutRooms || [],
-        polls: polls || [],
-        interpreters: interpreters || [],
-        passcode: hashedPasscode,
-        endDate,
-        updatedAt: Date.now(),
+        firstName, lastName, email, companyName, roles
       },
       { new: true }
     );
 
-    if (!updatedProject) {
-      return res.status(404).json({ message: "Project not found" });
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Contact not found" });
     }
 
-    res.status(200).json(updatedProject);
+    res.status(200).json(updatedContact);
   } catch (error) {
+    console.log('error in update contact function', error)
     res.status(500).json({ message: error.message });
   }
 };
--- src/api/routes/contactRoute.js
+++ src/api/routes/contactRoute.js
@@ -4,6 +4,6 @@ module.exports = function (app) {
   app.post("/api/create/contact", controller.createContact);
   app.get("/api/get/contact-id", controller.getContactById);
   app.get("/api/get-all/contact", controller.getAllContacts);
-  app.put("/api/update-contact", controller.updateContact);
+  app.put("/api/update-contact/:id", controller.updateContact);
   app.delete("/api/delete/contact/:id", controller.deleteContact);
 };
