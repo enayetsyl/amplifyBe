27f980a4d3c43df559570351b4acde748907ab51
enayetaitech <mdrehman@365aitech.com>
2024-08-15 09:26:10 +0600

    getContactsByUserId function and route done

--- src/api/controllers/contactController.js
+++ src/api/controllers/contactController.js
@@ -104,6 +104,28 @@ const deleteContact = async (req, res) => {
   }
 };
 
+const getContactsByUserId = async (req, res) => {
+  try {
+    const { id } = req.params;
+console.log(req.params)
+    if (!id) {
+      return res.status(400).json({ message: 'createdBy ID is required' });
+    }
+
+    const contacts = await Contact.find({createdBy:id });
+
+    if (contacts.length === 0) {
+      return res.status(404).json({ message: 'No contacts found for this user' });
+    }
+
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    res.status(500).json({ message: 'Server error, please try again later' });
+  }
+};
+
+
 // DELETE route
 
 module.exports = {
@@ -112,4 +134,5 @@ module.exports = {
   getContactById,
   updateContact,
   deleteContact,
+  getContactsByUserId
 };
--- src/api/routes/contactRoute.js
+++ src/api/routes/contactRoute.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
   app.post("/api/create/contact", controller.createContact);
   app.get("/api/get/contact-id", controller.getContactById);
   app.get("/api/get-all/contact", controller.getAllContacts);
+  app.get("/api/get-all/contact/:id", controller.getContactsByUserId);
   app.put("/api/update-contact/:id", controller.updateContact);
   app.delete("/api/delete/contact/:id", controller.deleteContact);
 };
