383ffa3ffe85fd574fc621fd46b44edf357a4ff8
Harsh356a <harsh@365aitech.com>
2024-09-11 16:12:37 +0530

    merged

--- index.js
+++ index.js
@@ -4,25 +4,21 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const { Timestamp } = require("mongodb");
+
 const app = express();
 const http = require("http").createServer(app);
-const jwt = require('jsonwebtoken');
-const cors = require("cors");
-const { Timestamp } = require('mongodb');
-
-
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-
 dotenv.config();
-app.use(cors({
-  origin: '*', 
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Import models
@@ -31,7 +27,6 @@ const Chat = require("./src/api/models/chatModelMesage.js");
 
 // Import routes
 const userRoutes = require("./src/api/routes/userMessRoutes.js");
-// const uploadFileRoutes = require("./src/api/routes/uploadFileRoute.js");
 
 // Import other route files
 require("./src/api/routes/userRoute.js")(app);
@@ -51,9 +46,9 @@ mongoose.set("strictQuery", false);
 
 // Mongoose connection options
 const options = {
-  serverSelectionTimeoutMS: 50000,  
-  socketTimeoutMS: 45000,           
-  connectTimeoutMS: 30000,         
+  serverSelectionTimeoutMS: 50000,
+  socketTimeoutMS: 45000,
+  connectTimeoutMS: 30000,
 };
 
 mongoose
@@ -65,49 +60,52 @@ mongoose
     console.error(`Error connecting to the database: ${err}`);
   });
 
-
-  // Middleware setup
-app.use(
-  session({
-    secret: "thisismysessionseceret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// Middleware setup
+app.use(session({
+  secret: "thisismysessionseceret",
+  resave: false,
+  saveUninitialized: true,
+}));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use the user routes
-// app.use("/api", uploadFileRoutes);
+// Serve static files
+app.use(express.static(path.join(__dirname, '../vite')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+// Route
+app.get('/ping', (req, res) => {
+  res.send({ success: true }).status(200);
+});
+
+let socketList = {};
 
 // Socket.IO namespace
-const usp = io.of("/user-namespace"); // Creating our own namespace for communication
+const usp = io.of("/user-namespace");
 
 usp.on("connection", async function (socket) {
-  console.log("User Connected");
+  console.log("User Connected to user-namespace");
 
   const userId = socket.handshake.auth.token;
-  // await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: "1" } });
 
-  // Broadcast the status of the user if they are online
   socket.broadcast.emit("getOnlineUser", { user_id: userId });
 
   socket.on("disconnect", async function () {
-    console.log("User Disconnected");
-    // await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: "0" } });
-
-    // Broadcast the status of the user if they are offline
+    console.log("User Disconnected from user-namespace");
     socket.broadcast.emit("getOfflineUser", { user_id: userId });
   });
 
-  // Chat implementation
   socket.on("newChat", function (data) {
-    console.log(data);
+    console.log("New chat in user-namespace:", data);
     socket.broadcast.emit("loadNewChat", data);
-    console.log(data, "88");
   });
 
-  // Load old chats
   socket.on("existChat", async function (data) {
     const chats = await Chat.find({
       $or: [
@@ -115,23 +113,93 @@ usp.on("connection", async function (socket) {
         { sender_id: data.receiver_id, receiver_id: data.sender_id },
       ],
     });
+    socket.emit("loadChats", { chats: chats });
+  });
+});
 
-    // Emit chats to frontend
-    socket.emit("loadChats", { chats: chats }, () => {
-      console.log(chats);
+// Main namespace
+io.on('connection', (socket) => {
+  console.log(`New User connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    socket.disconnect();
+    console.log('User disconnected!');
+  });
+
+  socket.on('BE-check-user', ({ roomId, userName }) => {
+    let error = false;
+    console.log('BE-check-user', roomId, userName);
+    io.sockets.in(roomId).clients((err, clients) => {
+      clients.forEach((client) => {
+        if (socketList[client] == userName) {
+          error = true;
+        }
+      });
+      socket.emit('FE-error-user-exist', { error });
     });
   });
-});
 
-io.on("connection", (socket) => {
-  console.log("User online");
+  socket.on('BE-join-room', ({ roomId, userName }) => {
+    console.log('BE-join-room', roomId, userName);
+    socket.join(roomId);
+    socketList[socket.id] = { userName, video: true, audio: true };
 
-  socket.on("canvas-data", (data) => {
-    socket.broadcast.emit("canvas-data", data);
+    io.sockets.in(roomId).clients((err, clients) => {
+      try {
+        const users = [];
+        clients.forEach((client) => {
+          users.push({ userId: client, info: socketList[client] });
+        });
+        socket.broadcast.to(roomId).emit('FE-user-join', users);
+      } catch (e) {
+        io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
+      }
+    });
+  });
+
+  socket.on('BE-call-user', ({ userToCall, from, signal }) => {
+    io.to(userToCall).emit('FE-receive-call', {
+      signal,
+      from,
+      info: socketList[socket.id],
+    });
+  });
+
+  socket.on('BE-accept-call', ({ signal, to }) => {
+    io.to(to).emit('FE-call-accepted', {
+      signal,
+      answerId: socket.id,
+    });
+  });
+
+  socket.on('BE-send-message', ({ roomId, msg, sender }) => {
+    io.sockets.in(roomId).emit('FE-receive-message', { msg, sender });
+  });
+
+  socket.on('BE-leave-room', ({ roomId, leaver }) => {
+    delete socketList[socket.id];
+    socket.broadcast
+      .to(roomId)
+      .emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
+    io.sockets.sockets[socket.id].leave(roomId);
+  });
+
+  socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
+    if (switchTarget === 'video') {
+      socketList[socket.id].video = !socketList[socket.id].video;
+    } else {
+      socketList[socket.id].audio = !socketList[socket.id].audio;
+    }
+    socket.broadcast
+      .to(roomId)
+      .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
+  });
+
+  socket.on('canvas-data', (data) => {
+    socket.broadcast.emit('canvas-data', data);
   });
 });
 
-// Start the server
 const PORT = process.env.PORT || 8008;
 http.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
