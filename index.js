79a21a0ef9b0de7184ca79ef9171012259273a59
Harsh356a <harsh@365aitech.com>
2024-09-26 16:06:38 +0530

    merge

--- index.js
+++ index.js
@@ -6,32 +6,34 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 const http = require("http").createServer(app);
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { Timestamp } = require('mongodb');
-
+const { Timestamp } = require("mongodb");
 
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-
 dotenv.config();
-app.use(cors({
-  origin: '*', 
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // Import models
 const User = require("./src/api/models/userModelMessage.js");
 const Chat = require("./src/api/models/chatModelMesage.js");
+const userRoleRoutes = require("./src/api/routes/userJoinMeetRoute.js");
 
 // Import routes
 const userRoutes = require("./src/api/routes/userMessRoutes.js");
 // const uploadFileRoutes = require("./src/api/routes/uploadFileRoute.js");
+app.use("/api", userRoleRoutes);
 
 // Import other route files
 require("./src/api/routes/userRoute.js")(app);
@@ -51,9 +53,9 @@ mongoose.set("strictQuery", false);
 
 // Mongoose connection options
 const options = {
-  serverSelectionTimeoutMS: 50000,  
-  socketTimeoutMS: 45000,           
-  connectTimeoutMS: 30000,         
+  serverSelectionTimeoutMS: 50000,
+  socketTimeoutMS: 45000,
+  connectTimeoutMS: 30000,
 };
 
 mongoose
@@ -65,8 +67,7 @@ mongoose
     console.error(`Error connecting to the database: ${err}`);
   });
 
-
-  // Middleware setup
+// Middleware setup
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -79,7 +80,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use the user routes
 // app.use("/api", uploadFileRoutes);
-
 
 // Start the server
 const PORT = process.env.PORT || 8008;
